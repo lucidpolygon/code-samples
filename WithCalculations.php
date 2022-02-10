299eac5b1c5c657f87091d1b626b6c31e8d46b81
lucidpolygon <40432572+lucidpolygon@users.noreply.github.com>
2022-02-10 08:09:49 +0400

    Traits to be shared among various modules
    This is a part of the software that does full-scale ERP functions such as Invoices, Bills, Returns, Taxes, and accounting.
    I wrote these two traits to be re-used among Invoices, Bills, and Returns.

---
+++ WithCalculations.php
@@ -0,0 +1,230 @@
+<?php
+
+namespace App\Http\Livewire\Documents\Traits;
+
+trait WithCalculations
+{
+    public $loopTotalDisc;
+    public $loopGrossTotal;
+    public $loopTotalTax;
+
+    public function isNumber($field, $index)
+    {
+        is_numeric($this->lines[$index][$field])
+            ? $this->lines[$index][$field] = round($this->lines[$index][$field], $this->settings['decimal'])
+            : $this->lines[$index][$field] = null;
+        return $this->lines[$index][$field];
+    }
+
+    public function calclateLineDiscount($index, $lineSubTotal)
+    {
+        $discValue = 0;
+        $percent = false;
+
+        $num = $this->lines[$index]['discount'];
+
+        if (substr($num, -1) == "%") {
+            $num = substr($num, 0, -1);
+            $percent = true;
+        }
+
+        if (is_numeric($num)) {
+            $num = round($num, $this->settings['decimal']);
+            $discValue = $num;
+
+            if ($percent) {
+                $discValue = $lineSubTotal * $num / 100;
+                $this->lines[$index]['discount'] = $num . '%';
+            } else {
+                $this->lines[$index]['discount'] = $num;
+            }
+        } else {
+            $this->lines[$index]['discount'] = null;
+        }
+
+        $discountAndTax = $this->header['discount_and_tax'];
+        $discountAndTax == "Line"
+            ? $discValue = round($discValue, $this->settings['decimal'])
+            : $discValue = 0;
+
+        $this->lines[$index]['discount_value'] = $discValue;
+        return $discValue;
+    }
+
+    public function calculateLineTotal($index)
+    {
+        $qty = $this->isNumber('qty', $index);
+        $rate = $this->isNumber('rate', $index);
+        $qty * $rate <> 0
+            ? $discValue = $this->calclateLineDiscount($index, $qty * $rate)
+            : $discValue = 0;
+        $this->lines[$index]['line_total'] = round(($qty * $rate) - $discValue, $this->settings['decimal']);
+        $this->calculateLineTax($index);
+    }
+
+    public function calculateLineTax($index)
+    {
+        $taxMethod = $this->header['tax_method'];
+        $discountAndTax = $this->header['discount_and_tax'];
+
+        $taxIndex = array_search($this->lines[$index]['tax_id'], array_column($this->lists['taxes'], 'id'));
+        $tax = $this->lists['taxes'][$taxIndex];
+        $taxRate = $tax['rate'];
+        $taxValue = 0;
+
+        $total = $this->lines[$index]['line_total'];
+
+        if ($total <> 0) {
+            if ($discountAndTax == "Line") {
+                if ($taxMethod == "Inclusive") {
+                    $taxValue = ($total / (100 + $taxRate)) * $taxRate;
+                }
+
+                if ($taxMethod == "Exclusive") {
+                    $taxValue = ($total / 100) * $taxRate;
+                }
+            }
+        }
+
+        $this->lines[$index]['tax_details'] = $tax;
+        $this->lines[$index]['tax_value'] = round($taxValue, $this->settings['decimal']);
+    }
+
+    public function loop()
+    {
+        $gt = 0;
+        $disc = 0;
+        $tax = 0;
+        foreach ($this->lines as $index => $row) {
+            $disc += $row['discount_value'];
+            $gt += $row['line_total'];
+            $tax += $row['tax_value'];
+        }
+        $this->loopGrossTotal = $gt;
+        $this->loopTotalDisc = $disc;
+        $this->loopTotalTax = $tax;
+    }
+
+    public function calculateNetTotal()
+    {
+        $discountAndTax = $this->header['discount_and_tax'];
+        $this->loop();
+
+        $this->header['discount_total'] = 0;
+        $this->header['tax_total'] = 0;
+
+        if ($discountAndTax == 'Line') {
+            $this->header['gross_total'] = $this->loopGrossTotal + $this->loopTotalDisc;
+            if ($this->header['gross_total'] <> 0) {
+                $this->header['discount_total'] = $this->loopTotalDisc;
+                $this->header['tax_total'] = $this->loopTotalTax;
+                $this->taxBreakdown = $this->calculateTaxBreakdown();
+            }
+        } else {
+            $this->header['gross_total'] = $this->loopGrossTotal;
+            if ($this->header['gross_total'] <> 0) {
+                $this->header['discount_total'] = $this->calculateTotalDiscount($this->header['discount'], $this->header['gross_total']);
+                $this->header['tax_total'] = $this->calculateTotalTax($this->header['gross_total'] - $this->header['discount_total']);
+                if ($this->header['tax_total'] > 0) {
+                    $this->taxBreakdown = [];
+                    $name = $this->header['tax_details']['name'] . " (" . $this->header['tax_details']['rate'] . "%)";
+                    $this->taxBreakdown[$name] =  $this->header['tax_total'];
+                }
+            }
+        }
+
+        $this->header['net_total'] = $this->header['gross_total'] - $this->header['discount_total'] + ($this->header['tax_method'] == "Inclusive" ? 0 : $this->header['tax_total']);
+    }
+
+
+    public function calculateTotalDiscount($totalDiscount, $grossTotal)
+    {
+        $discValue = 0;
+        $percent = false;
+        $num = $totalDiscount;
+
+        if (substr($num, -1) == "%") {
+            $num = substr($num, 0, -1);
+            $percent = true;
+        }
+
+        if (is_numeric($num)) {
+            $num = round($num, $this->settings['decimal']);
+            $discValue = $num;
+
+            if ($percent) {
+                $discValue = ($grossTotal / 100) * $num;
+                $this->header['discount'] = $num . '%';
+            } else {
+                $this->header['discount'] = $num;
+            }
+        } else {
+            $this->header['discount'] = null;
+        }
+
+        $this->header['discount_total'] = round($discValue, $this->settings['decimal']);
+        return round($discValue, $this->settings['decimal']);
+    }
+
+    public function calculateTotalTax($grossTotal)
+    {
+        $taxMethod = $this->header['tax_method'];
+        $taxIndex = array_search($this->header['tax_id'], array_column($this->lists['taxes'], 'id'));
+        $tax = $this->lists['taxes'][$taxIndex];
+
+        $taxRate = $tax['rate'];
+        $taxValue = 0;
+
+        if ($taxRate == 0) {
+            return 0;
+        }
+
+        if ($taxMethod == "Inclusive") {
+            $taxValue = ($grossTotal / (100 + $taxRate)) * $taxRate;
+        }
+        if ($taxMethod == "Exclusive") {
+            $taxValue = ($grossTotal / 100) * $taxRate;
+        }
+        $this->header['tax_details'] = $tax;
+        $this->header['tax_total'] = round($taxValue, $this->settings['decimal']);
+        return round($taxValue, $this->settings['decimal']);
+    }
+
+    public function calculateTaxBreakdown()
+    {
+        $unique = [];
+        $taxBreakdown = [];
+        foreach ($this->lines as $line) {
+            if ($line['tax_value'] <> 0) {
+                $name = $line['tax_details']['name'] . '-' . $line['tax_details']['rate'] . '%';
+                if (in_array($name, $unique)) {
+                    $taxBreakdown[$name] =  $taxBreakdown[$name] + $line['tax_value'];
+                } else {
+                    $taxBreakdown[$name] =  $line['tax_value'];
+                    $unique[] = $name;
+                }
+            }
+        }
+        return $taxBreakdown;
+    }
+
+    //Triggers
+
+    public function updatedHeaderDiscountAndTax()
+    {
+        foreach ($this->lines as $index => $line) {
+            $this->calculateLineTotal($index);
+            $this->calculateLineTax($index);
+        }
+        if ($this->header['discount_and_tax'] == "Total") {
+            $this->header['tax'] = $this->taxList->where('is_default', 1)->first()->id;
+        }
+    }
+
+    public function updatedHeaderTaxMethod()
+    {
+        foreach ($this->lines as $index => $line) {
+            $this->calculateLineTax($index);
+        }
+    }
+}
---
+++ WithDynamicForm.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace App\Http\Livewire\Documents\Traits;
+
+use App\Models\Tax;
+use App\Models\Item;
+use App\Models\Account;
+use App\Models\Contact;
+use App\Models\Setting;
+use Illuminate\Support\Facades\Config;
+
+trait WithDynamicForm
+{
+    public function getSettings($doc)
+    {
+        $settings = Setting::where('module', 'Accounting')
+            ->whereIn('setting', ['discount_and_tax', 'tax_method', 'currency'])
+            ->orwhereIn('module', [$doc])
+            ->get(['setting', 'value'])
+            ->toArray();
+        foreach ($settings as $key => $value) {
+            $this->settings[$value['setting']] = $value['value'];
+        }
+        $this->settings['decimal'] = Config::get('accounting.decimal');
+        $this->settings['document'] = $doc;
+    }
+
+    public function getLists($doc)
+    {
+        $this->taxList = Tax::get(['id', 'name', 'rate', 'is_default']);
+        $this->lists['taxMethods'] = ['Exclusive', 'Inclusive', 'No Tax'];
+        $this->lists['taxes'] = $this->taxList->toArray();
+
+        if ($doc == "Bill") {
+
+            $accounts = Account::whereIn(
+                'group',
+                ['Current Asset', 'Non-current Asset', 'Direct Cost', 'Other Expense']
+            )
+                ->orWhere('default_account', 'accounts_payable')
+                ->orWhere('default_account', 'rounding_difference')
+                ->get();
+            $this->settings['default_line_account'] = $accounts->where('default_account', 'inventory')->first()->id;
+        }
+        if ($doc == "Invoice" || $doc == "Quotation") {
+            $accounts = Account::whereIn('group', ['Current Liability', 'Non-current Liability', 'Income', 'Other Income'])
+                ->orWhere('default_account', 'accounts_receivable')
+                ->orWhere('default_account', 'inventory')
+                ->orWhere('default_account', 'discount_allowed')
+                ->orWhere('default_account', 'rounding_difference')
+                ->get();
+            $this->settings['default_line_account'] = $accounts->where('default_account', 'revenue')->first()->id;
+        }
+        $this->lists['accounts'] = $accounts;
+    }
+
+    public function assignTax()
+    {
+        $this->settings['discount_and_tax'] == "Total"
+            ? $tax = $this->taxList->where('is_default', 1)->first()->id
+            : $tax = null;
+        return $tax;
+    }
+
+    public function updatedHeaderDate()
+    {
+        $this->header['due_date'] =  $this->calculateDueDate();
+    }
+
+    public function calculateDueDate()
+    {
+        $date = date('Y-m-d', strtotime(str_replace('/', '-', $this->header['date'])));
+        $days = $this->settings['default_due_date_period'];
+        return  date('d/m/Y', strtotime($date . ' +' . $days . ' days'));
+    }
+
+    public function updatedSearchContact()
+    {
+        $this->header['contact_id'] = null;
+        $this->header['contact_name'] = null;
+        $this->reset('searchContactResults');
+        if (strlen($this->searchContact) > 0) {
+            $this->searchContactResults = Contact::search($this->searchContact)->get(['id', 'name'])->take(5);
+        }
+        if (count($this->searchContactResults) == 0) {
+            $this->emit('suggestedNewContact', $this->searchContact);
+        }
+    }
+
+    public function selectContact($id)
+    {
+        $name = $this->searchContactResults->find($id)->name;
+        $this->header['contact_id'] = $id;
+        $this->header['contact_name'] = $name;
+        $this->searchContact = $name;
+        $this->reset('searchContactResults');
+    }
+
+    //listening to contact creation via Livewire/Contacts/SimpleForm
+    public function contactCreated($contact)
+    {
+        $this->header['contact_id'] = $contact[0];
+        $this->header['contact_name'] = $contact[1];
+        $this->searchContact = $contact[1];
+    }
+
+    public function updatedSearchItem()
+    {
+        $this->reset('searchItemResults');
+        if (strlen($this->searchItem) > 0) {
+            if ($this->settings['document'] == "Invoice" || $this->settings['document'] == "Quotation") {
+                $this->searchItemResults = Item::search($this->searchItem)->get(['id', 'sku', 'title', 'selling_price'])->take(5);
+            }
+            if ($this->settings['document'] == "Bill") {
+                $this->searchItemResults = Item::search($this->searchItem)->hasCost()->get(['id', 'sku', 'title', 'selling_price'])->take(5);
+            }
+        }
+        if (count($this->searchItemResults) == 0) {
+            $this->emit('suggestedNewItem', $this->searchItem);
+        }
+    }
+
+    public function selectItem($id)
+    {
+        $item = $this->searchItemResults->find($id);
+        $this->searchItem = $item->name;
+        $this->addNewLine($item);
+        $this->reset('searchItemResults');
+    }
+
+    public function assignLineTax()
+    {
+        $this->header['discount_and_tax'] == "Line"
+            ? $tax = $this->taxList->where('is_default', 1)->first()->id
+            : $tax = null;
+        return $tax;
+    }
+
+    public function addNewLine($item)
+    {
+        $this->lines[] = [
+            'item_id' => $item->id,
+            'sku' => $item->sku,
+            'description' => $item->title,
+            'rate' => $this->settings['document'] <> "Bill" ? $item->selling_price : '',
+            'line_total' => $this->settings['document'] <> "Bill" ? $item->selling_price : 0,
+            'qty' => 1,
+            'discount' => null,
+            'discount_value' => 0,
+            'tax_id' => $this->assignLineTax(),
+            'tax_details' => null,
+            'tax_value' => 0,
+            'account_id' => $this->settings['document'] == "Invoice" ? $item->sales_account_id : $this->settings['default_line_account'],
+        ];
+        $this->linesDirty = true;
+    }
+
+    public function addBlankLine()
+    {
+        $this->lines[] = [
+            'item_id' => null,
+            'sku' => null,
+            'description' => '',
+            'qty' => '',
+            'rate' => '',
+            'line_total' => 0,
+            'discount' => null,
+            'discount_value' => 0,
+            'tax_id' => $this->assignLineTax(),
+            'tax_details' => null,
+            'tax_value' => 0,
+            'account_id' => $this->settings['default_line_account'],
+        ];
+        $this->dirty = true;
+    }
+
+    public function removeLine($id)
+    {
+        unset($this->lines[$id]);
+        $this->dirty = true;
+        $this->lines = array_values($this->lines);
+    }
+}
